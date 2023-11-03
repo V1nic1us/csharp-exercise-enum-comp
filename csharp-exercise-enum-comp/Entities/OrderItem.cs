44bac8d281b31b77390a1c9434a4d9c0c4565940
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-03 00:18:43 -0400

    feat: add order item class

---
+++ csharp-exercise-enum-comp/Entities/OrderItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_enum_comp.Entities
+{
+    internal class OrderItem
+    {
+        public int Quantity {  get; set; }
+        public double price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Product = product;
+            price = product.Price;
+        }
+
+        public double subTotal()
+        {
+            return price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product.Name}, R${price}, Quantity: {Quantity}, Subtotal: R${subTotal()}";
+        }
+    }
+}
