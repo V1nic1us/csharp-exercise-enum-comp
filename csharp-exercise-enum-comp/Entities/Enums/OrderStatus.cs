5528f9f9dea7f6e9445f53e8e3b28fb7cd1a69e3
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-03 00:19:51 -0400

    feat: add order status enum

---
+++ csharp-exercise-enum-comp/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_enum_comp.Entities.Enums
+{
+    internal enum OrderStatus: int
+    {
+        PEDING_PAYMENT = 0,
+        PROCESSING = 1,
+        SHIPPED = 2,
+        DELIVERED = 3,
+    }
+}
